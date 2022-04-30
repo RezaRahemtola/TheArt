8b5c8736e7a86c9739eb913767833c08bf9220bd
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-04-30 17:09:49 +0200

    feat: login and signup pages created

--- front/src/app/Routes.tsx
+++ front/src/app/Routes.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Redirect, Switch } from 'react-router-dom';
 
 import HomeView from 'views/HomeView';
+import SignupView from 'views/SignupView';
+import LoginView from 'views/LoginView';
 
 import AuthRoute from 'app/AuthRoute';
+
 
 const Routes = (): JSX.Element => (
 	<BrowserRouter>
 		<Switch>
 			<AuthRoute exact path="/" children={<HomeView />} />
+			<AuthRoute path="/signup" children={<SignupView />} />
+			<AuthRoute path="/login" children={<LoginView />} />
 			<Redirect push to="/" />
 		</Switch>
 	</BrowserRouter>
---
+++ front/src/views/SignupView.tsx
@@ -0,0 +1,9 @@
+import { Text } from '@chakra-ui/react';
+
+const SignupView = (): JSX.Element => (
+	<>
+		<Text>Signup page</Text>
+	</>
+);
+
+export default SignupView;
