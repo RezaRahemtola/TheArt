f5d6e4a2120eb5ad4aeaff59aa3ed12c1ff36b91
Lolboys <mateo.viel@epitech.eu>
2022-04-30 18:02:27 +0200

    feat: deploy script working

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,4 +1,13 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config()
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const API_KEY_TESTNET = process.env.API_KEY_TESTNET;
+const API_KEY_MAINET = process.env.API_KEY_MAINET;
+const URL_TESNET = process.env.URL_TESTNET;
+const URL_MAINET = process.env.URL_MAINET;
+
+console.log(PRIVATE_KEY, API_KEY_MAINET, API_KEY_TESTNET, URL_TESNET, URL_MAINET);
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +27,20 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  networks: {
+    matic: {
+      url: URL_MAINET + API_KEY_MAINET,
+      accounts: [`${PRIVATE_KEY}`]
+    },
+    mumbai: {
+      url: URL_TESNET + API_KEY_TESTNET,
+      accounts: [`${PRIVATE_KEY}`]
+    }
+  }
 };
