8e47d8c63f7690c6b6e6846e71c1a19b24f4b392
Lolboys <mateo.viel@epitech.eu>
2022-05-01 16:20:06 +0200

    remove: removed api directory and all its content

--- api/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-const express = require("express");
-const app = express();
-
-app.listen(process.env.PORT, () => {
-    console.log(`app listening at http://localhost: `, process.env.PORT);
-});
